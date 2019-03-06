ae3aa5ab15b0645e0d489a3c10cfa8d02f919907
incedius <negoki@gmail.com>
2019-03-06 16:47:17 +0800

    hello...

---
+++ README.md
@@ -0,0 +1,8 @@
+# Mel
+Hello...
+# channels.json
+ say = 0, party = 1, guild = 2, area = 3, trade = 4, greet = 9,
+ private = 11-18, p-notice = 21, emote = 26, global = 27, r-notice = 25,
+ raid = 32, megaphone = 213, guild-adv = 214
+
+Edit channels.json for channels you want the module enabled.
---
+++ index.js
@@ -0,0 +1,70 @@
+const path = require('path'),
+	  fs = require('fs')
+
+module.exports = function Mel(mod) {
+    let enabled = true,
+        channels,
+        fileopen=true,
+        stopwrite,
+        regex = new RegExp(/<FONT>(.*)<\/FONT>/)
+
+    try{
+        channels = JSON.parse(fs.readFileSync(path.join(__dirname,'channels.json'), 'utf8'))
+    }
+    catch(e){
+        msg("channels.json not found/malformed. Regenerating from default.")
+        channels = JSON.parse(fs.readFileSync(path.join(__dirname,'channels-default.json'), 'utf8'))
+        save(channels,"channels.json")
+    }
+
+    mod.command.add('mel', {
+        $none() {
+            enabled = !enabled
+            msg(enabled?"enabled":"disabled")
+        }
+    })
+
+
+    mod.hook('C_CHAT', 1, event => {
+        if (channels.includes(event.channel.toString())){
+            
+            mod.toServer('C_CHAT',1, {
+                channel: event.channel,
+                message: event.message.replace(regex, '<FONT>$1...</FONT>')
+            })
+
+            return false
+        }
+    })
+
+    mod.hook('C_WHISPER', 1, event => {
+    
+        mod.toServer('C_WHISPER',1, {
+            target: event.target,
+            message: event.message.replace(regex, '<FONT>$1...</FONT>')
+        })
+
+        return false
+        
+    })
+
+    function save(data, filename) {
+        if(fileopen) {
+            fileopen=false
+            fs.writeFile(path.join(__dirname, filename), JSON.stringify(data,null,"\t"), err => {
+                if(err){
+            mod.command.message('Error Writing File, attempting to rewrite')
+            console.log(err)
+        }
+                fileopen = true
+            })
+        }
+        else {
+            clearTimeout(stopwrite)			 //if file still being written
+            stopwrite=setTimeout(save(__dirname, filename),2000)
+            return
+        }
+    }
+
+    function msg(event) { mod.command.message(event); }
+}
